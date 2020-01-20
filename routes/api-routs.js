32a1470561aea10d7db35a3663575cef86d73c20
Tom Pocius <poci14@gmail.com>
2020-01-19 18:42:35 -0600

    framework and inital server working for fitness tracker

---
+++ routes/api-routs.js
@@ -0,0 +1,37 @@
+const Workout = require("./../models/workout");
+const router = require("express").Router();
+
+//add a new workout
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//add exercises to a workout
+//put route /api/workouts/:id
+//workout id with push to array
+
+
+//get all workouts
+// /api/workouts
+
+
+/* get specific workout 
+get last 7 workouts
+/api/workouts/range*/
+
+//deletee workout
+// /api/workouts
+
+
+
+
+
+
+module.exports = router;
