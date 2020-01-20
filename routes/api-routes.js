b143f1fda5d8d53e41754b9fce5b33d17c29a6af
Tom Pocius <poci14@gmail.com>
2020-01-19 19:54:25 -0600

    got post api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,37 +1,31 @@
-const Workout = require("../models/workout");
 const router = require("express").Router();
-
-//add a new workout
-
+const Workout = require("./../models/workout");
+/* add a new workout */
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
-    .then(dbworkout => {
-      res.json(dbworkout);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.json(err);
     });
 });
 
-//add exercises to a workout
-//put route /api/workouts/:id
-//workout id with push to array
-
-
-//get all workouts
-// /api/workouts
-
-
-/* get specific workout 
-get last 7 workouts
-/api/workouts/range*/
-
-//deletee workout
-// /api/workouts
-
-
-
-
-
-
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+/* add excercises to a work out */
+/* put route /api/workouts/:id */
+/* get all workouts */
+/* /api/workouts */
+/* get last 7 workout */
+/*  /api/workouts/range */
+/* delete workout */
+/* /api/workouts */
 module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const htmlRoute = require("./routes/html-routes");
 const userApiRoute = require("./routes/api-routes");
+const db = require("./models");
+const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
-
-const app = express();
 
 app.use(logger("dev"));
 
